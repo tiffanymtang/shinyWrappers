955a09c009bdaef6b39f2d8159609e2072a9f031
Tiffany Tang <tiffany.tang@berkeley.edu>
2023-12-30 16:16:50 -0600

    add pretty dashboard header

--- R/style.R
+++ R/style.R
@@ -26,6 +26,74 @@ use_pretty_style <- function() {
 }
 
 
+#' Pretty dashboard header
+#'
+#' @description Wrapper around `shinydashboard::dashboardHeader()` that allows
+#'  for the title to be positioned to the left or center.
+#'
+#' @inheritParams shinydashboard::dashboardHeader
+#' @param title_position Position of the title, either "left" or "center".
+#' @param title_style (Optional) list of CSS styling options for the title.
+#' @param ... Arguments passed to `shinydashboard::dashboardHeader()`.
+#'
+#' @export
+prettyDashboardHeader <- function(title, title_position = c("left", "center"),
+                                  title_style = list(), ...) {
+
+  title_position <- match.arg(title_position)
+  header <- shinydashboard::dashboardHeader(title = title, ...)
+
+  if (title_position == "left") {
+    style_ls <- list(
+      `font-weight` = "bold"
+    )
+  } else if (title_position == "center") {
+    style_ls <- list(
+      display = "block",
+      height = "50px",
+      `font-size` = "20px",
+      `line-height` = "50px",
+      `text-align` = "center",
+      `font-family` = "Helvetica Neue, Helvetica, Arial, sans-serif",
+      `font-weight` = "bold",
+      color = "white"
+    )
+  }
+  if (!is.null(title_style)) {
+    for (arg in names(title_style)) {
+      style_ls[[arg]] <- title_style[[arg]]
+    }
+  }
+  style <- do.call(css_styler, style_ls)
+
+  if (title_position == "left") {
+    header <- header %>%
+      htmltools::tagAppendAttributes(style = style, .cssSelector = ".logo")
+  } else if (title_position == "center") {
+    header <- header %>%
+      htmltools::tagAppendAttributes(
+        style = css_styler(`margin-left` = "0px"),
+        .cssSelector = ".navbar.navbar-static-top"
+      ) %>%
+      htmltools::tagAppendAttributes(
+        style = css_styler(position = "absolute", left = "0px"),
+        .cssSelector = ".sidebar-toggle"
+      )
+
+    title_tag_idx <- 2
+    title_tag <- header$children[[title_tag_idx]] %>%
+      htmltools::tagAppendAttributes(style = style)
+    title_tag$attribs$class <- NULL
+    header$children[[title_tag_idx]] <- NULL
+    header$children[[title_tag_idx]] <- htmltools::tagAppendChild(
+      header$children[[title_tag_idx]], title_tag
+    )
+  }
+
+  return(header)
+}
+
+
 #' Display inline block
 #'
 #' @param obj Object to display inline.
